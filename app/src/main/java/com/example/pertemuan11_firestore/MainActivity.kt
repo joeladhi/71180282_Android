5b213e643be5ee8c9247566015f0eed87d6fa333
Joel Adhi <joel.adhi@ti.ukdw.ac.id>
2021-12-16 04:21:42 +0700

    firestore

---
+++ app/src/main/java/com/example/pertemuan11_firestore/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.pertemuan11_firestore
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.FirebaseFirestoreKtxRegistrar
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    var firestore: FirebaseFirestore? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        firestore = FirebaseFirestore.getInstance()
+
+        val edtNama = findViewById<EditText>(R.id.edtNama)
+        val edtUsia = findViewById<EditText>(R.id.edtUsia)
+        val btnSimpan = findViewById<Button>(R.id.btnSimpan)
+
+        btnSimpan.setOnClickListener {
+            val penduduk = Penduduk(edtNama.text.toString(), edtUsia.text.toString().toInt())
+            firestore?.collection("penduduk")?.add(penduduk)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pertemuan11_firestore/Penduduk.kt
@@ -0,0 +1,3 @@
+package com.example.pertemuan11_firestore
+
+data class Penduduk ( val nama: String, val usia: Int)
