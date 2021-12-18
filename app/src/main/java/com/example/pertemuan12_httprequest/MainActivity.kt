ca55d8a6701fbf08de315ba1e69fc9ef36927e9c
Joel Adhi <joel.adhi@ti.ukdw.ac.id>
2021-12-18 10:57:13 +0800

    p12

---
+++ app/src/main/java/com/example/pertemuan12_httprequest/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.pertemuan12_httprequest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+
+import android.R
+import android.view.View
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Set the dimensions of the sign-in button.
+        // Set the dimensions of the sign-in button.
+
+
+
+        sign_in_button.visibility =  View.VISIBLE
+        signInButton.setSize(SignInButton.SIZE_STANDARD)
+    }
+
+}
